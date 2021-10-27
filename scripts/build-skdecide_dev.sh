6c2304c439002f12c5fb6a0d6d2f8be519631255
Emilien Despres <emilien.despres@gmail.com>
2021-10-27 16:04:35 +0200

    Pass boost libraries to docker
    Give a BOOST_ROOT env variable in Dockerfile_x86_64 to use the boost libraries installed in build.yml
    and remove from build-skdecide_dev.sh the re-installation of those libraries

--- scripts/build-skdecide_dev.sh
+++ scripts/build-skdecide_dev.sh
@@ -2,14 +2,6 @@
 set -ex
 
 yum install -y git zlib-devel
-# Install the latest boost library
-curl -L https://boostorg.jfrog.io/artifactory/main/release/1.76.0/source/boost_1_76_0.tar.gz -o boost-1.76.0.tar.gz \
-    && tar xfz boost-1.76.0.tar.gz \
-    && rm boost-1.76.0.tar.gz \
-    && cd boost_1_76_0 \
-    && ./bootstrap.sh --help \
-    && ./bootstrap.sh --without-icu --with-libraries=headers --prefix=/usr/local \
-    && ./b2 install
 
 for PYTHON_VER in 3.7 3.8; do
     python$PYTHON_VER -m pip install --upgrade -q pip
