65d607a1b44be62cafb81afcfca401efc4ac7223
Emilien Despres <emilien.despres@gmail.com>
2021-11-09 17:30:55 +0100

    Remove obsolete auditwheel install & options
    Remove the auditwheel installation since it is already
    included in manylinux docker images.
    
    Remove the PLAT variable for auditwheel repair --platform option
    since default is already manylinux_2_5_x86_64 (alias manylinux_1_x86_64).
    Also remove reference to PLAT in the comments about manual build

--- scripts/build-pip-install.sh
+++ scripts/build-pip-install.sh
@@ -14,5 +14,3 @@ for PYTHON_VER in 3.7 3.8; do
     python$PYTHON_VER -m pip install --upgrade -q pip
     python$PYTHON_VER -m pip install build cmake
 done
-#install auditwheel for python 3.8 (used after the build in scripts/build-manylinux-wheels.sh)
-python3.8 -m pip install -q auditwheel
